88437c6eb68927bbf25daa9226838ab10dd640b2
Anjola <anjioba@gmail.com>
2022-09-09 09:36:27 -0700

    Task 1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-
 /**
-*main - Enrty point
+*main - print the string that is in the put function
 *
-*Return: Always 0
+*Description: using the main function
+*this prints the program
+*Return: 0
 */
 int main(void)
 {
-	printf(" Size of char: %zu byte(s)\n", sizeof(char));
-	printf(" Size of int: %zu byte(s)\n", sizeof(int));
-	printf(" Size of long int: %lu byte(s)\n", sizeof(long int));
-	printf(" Size of long long int: %zu byte(s)\n", sizeof(long long int));
-	printf(" Size of float: %zu byte(s)\n", sizeof(float));
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
 
+	printf("Size of char: %lu byte(s)\n", sizeof(c));
+	printf("Size of int: %lu byte(s)\n", sizeof(i));
+	printf("Size of long int: %lu byte(s)\n", sizeof(li));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(lli));
+	printf("Size of float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
